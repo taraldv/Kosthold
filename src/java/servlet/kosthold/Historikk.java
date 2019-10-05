d5e73325f1f47684451d774b2f1aa5817bc608a9
taraldv <36439345+taraldv@users.noreply.github.com>
2019-10-05 02:52:29 +0200

    Update Historikk.java
    til ny versjon

--- src/java/servlet/kosthold/Historikk.java
+++ src/java/servlet/kosthold/Historikk.java
@@ -6,30 +6,36 @@
 package servlet.kosthold;
 
 import crypto.ValidSession;
+import html.StandardHtml;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import util.HTML;
 import util.http.Headers;
 
 /**
  *
  * @author
  */
-public class Historikk extends HttpServlet{
+public class Historikk extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         Headers.GET(resp);
         ValidSession.isValid(req, resp);
-        HTML html = new HTML("Kosthold Historikk");
-        html.addStandard();
-        html.addJS("../../js/kosthold.js");
-        html.addJS("../../js/kostholdHistorikk.js");
-        resp.getWriter().print(html.toString());
+        PrintWriter out = resp.getWriter();
+        try {
+            StandardHtml html = new StandardHtml("Kosthold Historikk");
+            String tableArr = "['getBrukerMatvaretabell','brukerMatvaretabellTabell','/kosthold/matvaretabellen/']";
+            String deleteArr = "['deleteMatvare','matvareId','/kosthold/matvaretabellen/']";
+            html.addBodyJS("buildTable(" + tableArr + "," + deleteArr + ",0);");
+            out.print(html.toString());
+        } catch (Exception e) {
+            e.printStackTrace(out);
+        }
 
     }
 }
